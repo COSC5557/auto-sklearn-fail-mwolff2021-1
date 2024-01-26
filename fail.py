b0716351a589ca4394610169759f6a03bf955d45
mwolff2021 <42896248+mwolff2021@users.noreply.github.com>
2024-01-25 21:02:01 -0700

    Add files via upload

--- fail.py
+++ fail.py
@@ -4,6 +4,10 @@ import sklearn.metrics
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder
 
+#https://www.analyticsvidhya.com/blog/2020/07/10-techniques-to-deal-with-class-imbalance-in-machine-learning/
+#https://machinelearningmastery.com/tactics-to-combat-imbalanced-classes-in-your-machine-learning-dataset/
+#https://datagy.io/python-zip-lists/
+
 X, y = fetch_openml(data_id=40691, as_frame=True, return_X_y=True)
 print(X)
 #enc = OneHotEncoder(handle_unknown='ignore')
@@ -21,20 +25,49 @@ print(X)
 #original code
 X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, random_state=42)
 
-clf = RandomForestClassifier(random_state=42)
-clf = clf.fit(X_train, y_train)
-y_hat = clf.predict(X_test)
-print(y_hat)
-print("RF Accuracy", sklearn.metrics.accuracy_score(y_test, y_hat))
+#correct class imbalance in training data 
+import imblearn
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler, SMOTE
 
-from autosklearn.classification import AutoSklearnClassifier
+rus = RandomUnderSampler(random_state=42, replacement=True)# fit predictor and target variable
+x_rus, y_rus = rus.fit_resample(X_train, y_train)
 
-automl = AutoSklearnClassifier(time_left_for_this_task=300)
-automl.fit(X_train, y_train)
-y_hat = automl.predict(X_test)
-print(y_hat)
-print("AutoML Accuracy", sklearn.metrics.accuracy_score(y_test, y_hat))
-#end original code
+ros = RandomOverSampler(random_state=42)
+x_ros, y_ros = ros.fit_resample(X_train, y_train)
 
-print(automl.leaderboard())
-print(automl.show_models())
+smote = SMOTE()
+
+x_smote, y_smote = smote.fit_resample(X_train, y_train)
+
+x_train_datasets = [X_train, x_rus, x_ros, x_smote]
+y_train_datasets = [y_train, y_rus, y_ros, y_smote]
+
+balanced_datasets = list(zip(x_train_datasets, y_train_datasets))
+
+def compare_classifiers(X_train, y_train, X_test, y_test): 
+    #original code
+    clf = RandomForestClassifier(random_state=42)
+    clf = clf.fit(X_train, y_train)
+    y_hat = clf.predict(X_test)
+    print(y_hat)
+    print("RF Accuracy", sklearn.metrics.accuracy_score(y_test, y_hat))
+    
+    from autosklearn.classification import AutoSklearnClassifier
+    
+    automl = AutoSklearnClassifier(time_left_for_this_task=300)
+    automl.fit(X_train, y_train)
+    y_hat = automl.predict(X_test)
+    print(y_hat)
+    print("AutoML Accuracy", sklearn.metrics.accuracy_score(y_test, y_hat))
+    #end original code
+
+x_pos = 0
+y_pos = 1
+
+for i in range(0, len(balanced_datasets)): 
+    compare_classifiers(balanced_datasets[i][x_pos], balanced_datasets[i][y_pos], X_test, y_test)
+
+#vestigial code
+#print(automl.leaderboard())
+#print(automl.show_models())
--- fail.sh
+++ fail.sh
@@ -3,7 +3,7 @@
 ##SBATCH --job-name pml_hpo
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=mwolff3@uwyo.edu
-#SBATCH --time=0-0:15:00
+#SBATCH --time=0-01:00:00
 #SBATCH --mem=32GB
-python3 /home/mwolff3/fail.py > fail_2.out
+python3 /home/mwolff3/fail.py > fail_3.out
 
